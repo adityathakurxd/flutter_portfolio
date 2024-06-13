e4f6128ba6f68d233c2a5054c8f075c7a337e0b3
Aditya Thakur <adityathakurxd@gmail.com>
2024-06-13 22:01:09 +0530

    Updates the bootstrap file and hosts latest

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,15 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+const loading = document.createElement('div');
+document.body.appendChild(loading);
+loading.textContent = "Loading Entrypoint...";
+_flutter.loader.load({
+  onEntrypointLoaded: async function(engineInitializer) {
+    loading.textContent = "Initializing engine...";
+    const appRunner = await engineInitializer.initializeEngine();
+
+    loading.textContent = "Running app...";
+    await appRunner.runApp();
+  }
+});
